3bc0df789bfc980de35ccd65393596f14c7c0826
Vishal Katti <katti.vishal@gmail.com>
2025-05-22 17:31:59 +0530

    added Auth0 for authentication

--- R/app.R
+++ R/app.R
@@ -1,31 +1,32 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(RMySQL)
 library(pool)
+library(auth0)
+
+options(shiny.port = 8080)
 
 config <- config::get(config = "REMOTE") # Enable this when testing in AWS RDS
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("AWS Connection Test with Docker"),
+    titlePanel("AWS Connection Test with Docker and Auth0"),
 
     fluidRow(
-        tableOutput("table")
+        column(
+            width=10,
+            tableOutput("table")
+            ),
+        column(width=2,
+               actionButton("logout", "Logout", icon = icon("right-from-bracket"), class = "btn btn-danger")
+        )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     
     output$table <- renderTable({
         # Create a connection pool
@@ -47,8 +48,13 @@ server <- function(input, output) {
         # Return the result as a table
         result
     })
+    
+    observeEvent(input$logout, {
+        # Logout the user
+        auth0::logout()
+    })
 
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyAppAuth0(ui = ui, server = server)
--- R/setup.R
+++ R/setup.R
@@ -1,2 +1,2 @@
 # This file installs the required libraries
-install.packages(c('shiny','RMySQL','pool','config', 'DBI'), repos = 'https://cloud.r-project.org/')
+install.packages(c('shiny','RMySQL','pool','config', 'DBI','auth0'), repos = 'https://cloud.r-project.org/')
